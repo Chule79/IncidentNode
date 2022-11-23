569b33f0151e67d62ed5d0d9ca82e997b59484e1
Lauris-21 <laurasanchez0301@gmail.com>
2022-11-23 19:03:31 +0100

    orm-notice

---
+++ src/domain/orm/orm-department.js
@@ -0,0 +1,27 @@
+const conn = require('../repostories/mongo.repository');
+const magic = require('../../utils/magic');
+
+exports.GetAll = async () => {
+  try {
+    return await conn.db.connMongo.Department.find();
+  } catch (error) {
+    magic.LogDanger('Cannot getAll departments', error);
+    return await { err: { code: 123, message: error } };
+  }
+};
+
+exports.Create = async (Name, Users, Incidents, Notices) => {
+  try {
+    const data = await new conn.db.connMongo.Department({
+      name: Name,
+      users: Users,
+      incidents: Incidents,
+      notices: Notices,
+    });
+    data.save();
+    return true;
+  } catch (error) {
+    magic.LogDanger('Cannot create department', error);
+    return await { err: { code: 123, message: error } };
+  }
+};
