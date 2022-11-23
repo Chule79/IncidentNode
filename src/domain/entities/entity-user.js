b8218982ecbc8b64f74306666843ae4a9077e53a
Iker-Romero <ikersoftdev@gmail.com>
2022-11-23 17:52:42 +0100

    User Model

---
+++ src/domain/entities/entity-user.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+module.exports = (db) => {
+  const userSchema = new db.Schema(
+    {
+      username: { type: String, required: true },
+      nickname: { type: String, required: true, unique: true },
+      gmail: { type: String, required: true, unique: true },
+      password: { type: String, required: true },
+      role: { type: String, required: true },
+      image: { type: String },
+      department: { type: String, required: true },
+      incidents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'incident' }],
+    },
+    {
+      timestamps: true,
+    }
+  );
+  return db.model('user', userSchema);
+};
