9f1a3a8aa0dd30213530015d43b97b4eea259f56
Iker-Romero <ikersoftdev@gmail.com>
2022-11-23 21:23:19 +0100

    Feat: Error model in service-user

---
+++ src/domain/services/service-user.js
@@ -0,0 +1,80 @@
+const magic = require('../../utils/magic');
+const enum_ = require('../../utils/enum');
+const ormUser = require('../../domain/orm/orm-user');
+
+exports.GetAll = async (req, res) => {
+  let status = 'Success';
+  let errorcode = '';
+  let message = '';
+  let data = '';
+  let statuscode = 0;
+  let response = {};
+  try {
+    let resOrm = await ormUser.GetAll();
+    if (resOrm.err) {
+      (status = 'Failure'),
+        (errorcode = respOrm.err.code),
+        (message = respOrm.err.message),
+        (statuscode = enum_.CODE_BAD_REQUEST);
+    } else {
+      (message = 'Success GetAll users'),
+        (data = respOrm),
+        (statuscode = data.length > 0 ? enum_.CODE_OK : enum_.CODE_NO_CONTENT);
+    }
+    response = await magic.ResponseService(status, errorcode, message, data);
+    return res.status(statuscode).send(response);
+  } catch (error) {
+    magic.LogDanger('error: ', error);
+    response = await magic.ResponseService(
+      'Failure',
+      enum_.CODE_BAD_REQUEST,
+      error,
+      ''
+    );
+    return res.status(enum_.CODE_INTERNAL_SERVER_ERROR).send(response);
+  }
+};
+
+exports.Create = async (req, res) => {
+  let status = 'Success',
+    errorcode = '',
+    message = '',
+    data = '',
+    statuscode = 0,
+    response = {};
+  try {
+    const { username, nickname, gmail, password, role, departmen } = req.body;
+    if (username && nickname && gmail && password && role && department) {
+      let resOrm = await ormUser.Create(
+        username,
+        nickname,
+        gmail,
+        password,
+        role,
+        departmen
+      );
+      if (resOrm.err) {
+        (status = 'Failure'),
+          (errorcode = resOrm.err.code),
+          (message = resOrm.err.messsage),
+          (statuscode = enum_.CODE_BAD_REQUEST);
+      } else {
+        (message = 'User created'), (statuscode = enum_.CODE_CREATED);
+      }
+    } else {
+      (status = 'Failure'),
+        (errorcode = enum_.ERROR_REQUIRED_FIELD),
+        (message = 'Required fields incompleted'),
+        (statuscode = enum_.CODE_BAD_REQUEST);
+    }
+    response = await magic.ResponseService(status, errorcode, message, data);
+    return res.status(statuscode).send(response);
+  } catch (error) {
+    console.log('err = ', err);
+    return res
+      .status(enum_.CODE_INTERNAL_SERVER_ERROR)
+      .send(
+        await magic.ResponseService('Failure', enum_.CRASH_LOGIC, 'err', '')
+      );
+  }
+};
