3e34730e7e80e658ba04d9c91ccdaee8b110f004
Lauris-21 <laurasanchez0301@gmail.com>
2022-11-23 17:35:41 +0100

    entity-department

---
+++ src/domain/entities/entity-department.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+module.exports = (db) => {
+  const departmentSchema = new db.Schema(
+    {
+      name: { type: String, required: true, unique: true },
+      users: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'user',
+          required: true,
+        },
+      ],
+      incidents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'incident' }],
+      notices: [{ type: mongoose.Schema.Types.ObjectId, ref: 'notice' }],
+    },
+    {
+      timestamps: true,
+    }
+  );
+  return db.model('department', departmentSchema);
+};
