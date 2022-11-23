e21e1a58167be20a66338bea8fae50e966053abe
Lauris21 <112576133+Lauris21@users.noreply.github.com>
2022-11-23 19:05:25 +0100

    orm-notice (#14)

---
+++ src/domain/orm/orm-notice.js
@@ -0,0 +1,26 @@
+const conn = require('../repostories/mongo.repository');
+const magic = require('../../utils/magic');
+
+exports.GetAll = async () => {
+  try {
+    return await conn.db.connMongo.Notice.find();
+  } catch (error) {
+    magic.LogDanger('Cannot getAll notices', error);
+    return await { err: { code: 123, message: error } };
+  }
+};
+
+exports.Create = async (Title, Description, Departments) => {
+  try {
+    const data = await new conn.db.connMongo.Notice({
+      title: Title,
+      description: Description,
+      departments: Departments,
+    });
+    data.save();
+    return true;
+  } catch (error) {
+    magic.LogDanger('Cannot create notice', error);
+    return await { err: { code: 123, message: error } };
+  }
+};
