3c943d01ecee8bafbe0d818f67c1f340cc9d26e7
Iker-Romero <113237830+Iker-Romero@users.noreply.github.com>
2022-11-24 17:21:25 +0100

    Login fixes (#34)

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,8 +3,10 @@ const user = require('../domain/services/service-user');
 const department = require('../domain/services/service-department');
 const notice = require('../domain/services/service-notice');
 const incident = require('../domain/services/service-incident');
+const isAuth = require('../utils/middlewares/auth-middleware');
 
-router.post('/users', user.Create);
+router.post('/users/register', user.Create);
+router.post('/users/login', user.Login);
 router.get('/users', user.GetAll);
 router.post('/departments', department.Create);
 router.get('/departments', department.GetAll);
