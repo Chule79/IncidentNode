f1b94cad2bd79b807093091d327f8c218699ad62
Lauris-21 <laurasanchez0301@gmail.com>
2022-11-24 21:46:52 +0100

    Cloudinary-page

--- src/controller/index.js
+++ src/controller/index.js
@@ -4,6 +4,7 @@ const department = require('../domain/services/service-department');
 const notice = require('../domain/services/service-notice');
 const incident = require('../domain/services/service-incident');
 const isAuth = require('../utils/middlewares/auth-middleware');
+const upload = require('../utils/middlewares/file');
 
 router.post('/users/register', user.Create);
 router.post('/users/login', user.Login);
@@ -15,7 +16,7 @@ router.patch('/departments/:id', department.Update);
 router.delete('/departments/:id', department.Delete);
 router.post('/notices', notice.Create);
 router.get('/notices', notice.GetAll);
-router.post('/incidents', incident.Create);
+router.post('/incidents', upload.single('photos'), incident.Create);
 router.get('/incidents', incident.GetAll);
 router.get('/notices/:id', notice.GetOne);
 router.patch('/notices/:id', notice.Update);
--- src/domain/orm/orm-incident.js
+++ src/domain/orm/orm-incident.js
@@ -1,58 +1,60 @@
 const conn = require('../repositories/mongo.repository');
 const magic = require('../../utils/magic');
 
-
 const db = conn.db.connMongo;
 
 exports.GetAll = async () => {
   try {
-    return await db.Incident.find().populate("responsibles user department");
+    return await db.Incident.find().populate('responsibles user department');
   } catch (err) {
     magic.LogDanger('Cannot getAll incidents', err);
     return await { err: { code: 123, message: err } };
   }
-}; 
-
+};
 
 exports.GetOne = async (req) => {
   try {
     const { id } = req.params;
-    const incident = await db.Incident.findById(id).populate("responsibles user department");
-    return incident
+    const incident = await db.Incident.findById(id).populate(
+      'responsibles user department'
+    );
+    return incident;
   } catch (error) {
     magic.LogDanger('Cannot getAll incidents', err);
-    return  { err: { code: 123, message: err } };
+    return { err: { code: 123, message: err } };
   }
 };
 
 exports.Update = async (req) => {
   try {
     const { id } = req.params;
-    const incident = new db.Incident(req.body)
-    incident._id=id;
-    const updateIncident = await db.Incident.findByIdAndUpdate(id, incident)
-    return updateIncident 
+    const incident = new db.Incident(req.body);
+    incident._id = id;
+    const updateIncident = await db.Incident.findByIdAndUpdate(id, incident);
+    return updateIncident;
   } catch (error) {
     magic.LogDanger('Cannot getAll incidents', err);
-    return  { err: { code: 123, message: err } };
+    return { err: { code: 123, message: err } };
   }
 };
 
 exports.Delete = async (req) => {
   try {
     const { id } = req.params;
-    const updateIncident = await db.Incident.findByIdAndDelete(id)
-    return updateIncident 
+    const updateIncident = await db.Incident.findByIdAndDelete(id);
+    return updateIncident;
   } catch (error) {
     magic.LogDanger('Cannot getAll incidents', err);
-    return  { err: { code: 123, message: err } };
+    return { err: { code: 123, message: err } };
   }
 };
 
-
-exports.Create = async (info) => {
+exports.Create = async (req) => {
   try {
-    const newIncident = new db.Incident(info);
+    const newIncident = new db.Incident(req);
+    if (req.file) {
+      newIncident.photos = req.file.path;
+    }
     const savedIncident = await newIncident.save();
     return savedIncident;
   } catch (err) {
