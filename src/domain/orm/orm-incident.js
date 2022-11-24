b9eba417e18cf69d9007ea02e86de533b9fa6553
Jesus Santos Lopez <83368883+Chule79@users.noreply.github.com>
2022-11-24 17:35:17 +0100

    feat: Metodos terminados (#35)

--- src/domain/orm/orm-incident.js
+++ src/domain/orm/orm-incident.js
@@ -1,6 +1,7 @@
 const conn = require('../repositories/mongo.repository');
 const magic = require('../../utils/magic');
 
+
 const db = conn.db.connMongo;
 
 exports.GetAll = async () => {
@@ -10,7 +11,44 @@ exports.GetAll = async () => {
     magic.LogDanger('Cannot getAll incidents', err);
     return await { err: { code: 123, message: err } };
   }
+}; 
+
+
+exports.GetOne = async (req) => {
+  try {
+    const { id } = req.params;
+    const incident = await db.Incident.findById(id).populate("responsibles user department");
+    return incident
+  } catch (error) {
+    magic.LogDanger('Cannot getAll incidents', err);
+    return  { err: { code: 123, message: err } };
+  }
 };
+
+exports.Update = async (req) => {
+  try {
+    const { id } = req.params;
+    const incident = new db.Incident(req.body)
+    incident._id=id;
+    const updateIncident = await db.Incident.findByIdAndUpdate(id, incident)
+    return updateIncident 
+  } catch (error) {
+    magic.LogDanger('Cannot getAll incidents', err);
+    return  { err: { code: 123, message: err } };
+  }
+};
+
+exports.Delete = async (req) => {
+  try {
+    const { id } = req.params;
+    const updateIncident = await db.Incident.findByIdAndDelete(id)
+    return updateIncident 
+  } catch (error) {
+    magic.LogDanger('Cannot getAll incidents', err);
+    return  { err: { code: 123, message: err } };
+  }
+};
+
 
 exports.Create = async (info) => {
   try {
