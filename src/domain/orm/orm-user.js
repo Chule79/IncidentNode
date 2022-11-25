58f7bd813f5e3ff94effd1b54bf2a1b4b45d7a88
Iker-Romero <113237830+Iker-Romero@users.noreply.github.com>
2022-11-25 12:41:35 +0100

    Roles implemented in all endpoints with error handler improved (#47)
    * User endpoints
    
    * Roles implemented in all endpoints with error handler improved

--- src/domain/orm/orm-user.js
+++ src/domain/orm/orm-user.js
@@ -34,18 +34,36 @@ exports.Login = async (req) => {
     if (!userInDB) return magic.LogDanger("Login credentials doesn't exist");
 
     if (bcrypt.compareSync(req.body.password, userInDB.password)) {
+      console.log('password');
       userInDB.password = null;
-      const token = jwt.sign(
+      const userToken = jwt.sign(
         {
           id: userInDB._id,
           nickname: userInDB.nickname,
+          role: userInDB.role,
         },
-        req.app.get('secretKey'),
+        req.app.get('userSecretKey'),
         {
           expiresIn: '1h',
         }
       );
-      return { user: userInDB, token: token };
+      const adminToken = jwt.sign(
+        {
+          id: userInDB._id,
+          nickname: userInDB.nickname,
+          role: userInDB.role,
+        },
+        req.app.get('adminSecretKey'),
+        {
+          expiresIn: '1h',
+        }
+      );
+      console.log(userToken, adminToken);
+      if (userInDB.role === 'admin') {
+        return { user: userInDB, token: adminToken };
+      } else {
+        return { user: userInDB, token: userToken };
+      }
     } else {
       return next('User password incorrect');
     }
--- src/utils/helpers/error.js
+++ src/utils/helpers/error.js
@@ -1,5 +1,6 @@
 const setError = (code, message) => {
-  const err = new err();
+  console.log(code, message);
+  const err = new Error();
   err.code = code;
   err.message = message;
   return err;
