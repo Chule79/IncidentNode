a15ff64821b56ecac9a645ef14f827f5eeb9d1aa
Chule79 <jsantoslopez99@gmail.com>
2022-11-26 11:09:50 +0100

    fear: comienzo de home

--- app/src/routes/Home.jsx
+++ app/src/routes/Home.jsx
@@ -18,7 +18,41 @@ const Home = () => {
           display="flex"
           justify="center"
           align="center"
-        ></Div>
+          flexdir="column"
+        >
+          <img src="https://res.cloudinary.com/dcssmtpvq/image/upload/v1669457199/incidencias_zjf8ew.png"/>
+          <form className="formInicio">
+            <div className="divName">
+              <label htmlFor="name">
+                <a className="asterisco">*</a> Nombre de usuario:
+              </label>
+              <input type="text" name="name" className="inputsInicio" />
+            </div>
+            <div className="divName">
+              <label htmlFor="passw">
+                <a className="asterisco">*</a> Contraseña:
+              </label>
+              <input
+                type="password"
+                name="passw"
+                className="inputsInicio"
+                id="password"
+              />
+            </div>
+
+            <div className="formOpciones">
+              <div className="rememberMe">
+                <input type="checkbox" name="remember" className="checkIni" />
+                <label htmlFor="remember">Remember me</label>
+              </div>
+              <div>
+                <a className="olvide">Olvidè Contraseña</a>
+              </div>
+            </div>
+
+            <input type="submit" value="Iniciar sesión" className="botonInic"></input>
+          </form>
+        </Div>
       </Div>
     </>
   );
