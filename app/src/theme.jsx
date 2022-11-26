3fb012509f23983daf62a841dc3abd7ab09ec95d
Iker-Romero <ikersoftdev@gmail.com>
2022-11-26 15:49:38 +0100

    Theme update

--- app/src/theme.jsx
+++ app/src/theme.jsx
@@ -1,9 +1,18 @@
 const theme = {
-  primary: '',
-  secondary: '',
-  highlight: '',
-  background: '',
-  popUpBackground: 'rgba(0, 0, 0, 0)',
+  light: {
+    primary: '#242729',
+    secondary: '#393e43',
+    highlight: '#3f8ea5',
+    background: '#cbd9d9',
+    popUpBackground: 'rgba(36, 39, 41, 0.66)',
+  },
+  dark: {
+    primary: '#aeb4c3',
+    secondary: '#e9ddb8',
+    highlight: '#3f8ea5',
+    background: '#062c44',
+    popUpBackground: 'rgba(6, 44, 68, 0.66)',
+  },
 };
 
 export default theme;
