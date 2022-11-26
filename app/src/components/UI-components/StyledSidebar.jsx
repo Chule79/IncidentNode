79ca195401f5102aae8f8f1fc068f25eec608bba
Lauris-21 <laurasanchez0301@gmail.com>
2022-11-26 16:17:28 +0100

    Sidebar

---
+++ app/src/components/UI-components/StyledSidebar.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const SidebarStyle = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 20%;
+  background-color: grey;
+  display: flex;
+  flex-direction: column;
+  flex: 0 1 4rem;
+  justify-content: space-evenly;
+  align-items: start;
+  padding: 0.5rem;
+  gap: 1rem;
+  padding: 2rem;
+`;
+
+export default SidebarStyle;
