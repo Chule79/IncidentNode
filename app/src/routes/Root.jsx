8e35c137200e024d2ab152848001376edb6cf06f
Iker-Romero <ikersoftdev@gmail.com>
2022-11-26 17:15:25 +0100

    Navbar

--- app/src/routes/Root.jsx
+++ app/src/routes/Root.jsx
@@ -1,8 +1,44 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
+
+import Nav from '../components/UI-components/StyledNav';
+import Div from './../components/UI-components/StyledDiv';
+import StyledInput from './../components/UI-components/StyledInput';
 
 const Root = () => {
   return (
     <>
+      <Nav>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="#000000"
+          viewBox="0 0 30 30"
+          width="2rem"
+          height="2rem"
+        >
+          <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z" />
+        </svg>
+        <Div display="flex" flex="1 1 auto">
+          <StyledInput
+            placeholder="Search"
+            width="100%"
+            height="2rem"
+            border="1px solid black"
+            borderradius="1rem"
+            padding="1rem"
+          />
+        </Div>
+        <Div display="flex">
+          <svg viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
+            <path d="M256 32V49.88C328.5 61.39 384 124.2 384 200V233.4C384 278.8 399.5 322.9 427.8 358.4L442.7 377C448.5 384.2 449.6 394.1 445.6 402.4C441.6 410.7 433.2 416 424 416H24C14.77 416 6.365 410.7 2.369 402.4C-1.628 394.1-.504 384.2 5.26 377L20.17 358.4C48.54 322.9 64 278.8 64 233.4V200C64 124.2 119.5 61.39 192 49.88V32C192 14.33 206.3 0 224 0C241.7 0 256 14.33 256 32V32zM216 96C158.6 96 112 142.6 112 200V233.4C112 281.3 98.12 328 72.31 368H375.7C349.9 328 336 281.3 336 233.4V200C336 142.6 289.4 96 232 96H216zM288 448C288 464.1 281.3 481.3 269.3 493.3C257.3 505.3 240.1 512 224 512C207 512 190.7 505.3 178.7 493.3C166.7 481.3 160 464.1 160 448H288z" />
+          </svg>
+          <Div
+            height="2rem"
+            width="2rem"
+            border="1px solid black"
+            bordradius="1rem"
+          ></Div>
+        </Div>
+      </Nav>
       <main>
         <Outlet />
       </main>
