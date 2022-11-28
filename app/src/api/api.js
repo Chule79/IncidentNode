8aaa95f63582c90852bd36b23893eef020e3110a
Chule79 <jsantoslopez99@gmail.com>
2022-11-28 20:31:09 +0100

    readme

--- app/src/api/api.js
+++
@@ -1,7 +0,0 @@
-const getData = async (filtro) => {
-  const data = await fetch(`https://incidentnode-back.onrender.com/api/v1/${filtro}`);
-  const res = await data.json();
-  return res;
-};
-
-export default getData;
--- app/src/components/IncidentDiv.jsx
+++
@@ -1,8 +0,0 @@
-import H2 from './ModelTitle';
-import ModelDivInfo from './UI-components/ModelDivInfo';
-
-const IncidentDiv = () => {
-  return <ModelDivInfo></ModelDivInfo>;
-};
-
-export default IncidentDiv;
